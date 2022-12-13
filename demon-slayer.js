1757563375d9fec53d1614c3fe61ba306efc63db
after3gamedesign <110561244+after3gamedesign@users.noreply.github.com>
2022-12-13 16:16:35 -0500

    Create demon-slayer.js

---
+++ demon-slayer.js
@@ -0,0 +1,6 @@
+
+ 
+ // Type some code on a new line (such as "osc().out()"), and press CTRL+shift+enter
+s0.initVideo("https://media4.giphy.com/media/J6JazAkCVLId91L4yM/giphy.mp4")
+s1.initVideo("https://media4.giphy.com/media/Qx5C3JmHhyabwRqsOo/giphy.mp4")
+src(s0).pixelate(100,100).blend(s1).out(o0)
